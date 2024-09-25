4bf273ffa18862c351743fbda2ee5d2d75fef81f
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-09-25 20:18:41 +0530

    Creatting Pages and setting Routing _ currently have error but will fix

---
+++ coach/src/router.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import CoachesDetails from "./pages/coaches/CoachesDetails.vue";
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
+import ContactCoach from "./pages/request/ContactCoach.vue";
+import RequestReceived from "./pages/request/RequestReceived.vue";
+import NotFound from "./pages/NotFound.vue";
+
+const router = createRouter({
+  history: createWebHistory,
+  routes: [
+    {
+      path: "/",
+      redirect: "/coaches",
+    },
+    {
+      path: "/coaches",
+      component: CoachesList,
+    },
+    {
+      path: "/coaches/:id",
+      component: CoachesDetails,
+      children: [
+        {
+          path: "contact",
+          component: ContactCoach,
+        },
+      ],
+    },
+    {
+      path: "/register",
+      component: CoachRegistration,
+    },
+    {
+      path: "/requests",
+      component: RequestReceived,
+    },
+    {
+      path: "/:notFound(.*)",
+      component: NotFound,
+    },
+  ],
+});
