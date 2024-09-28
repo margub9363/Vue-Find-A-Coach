011cfed540c1afda5d61eb67b485058755702744
margub9363 <69838623+margub9363@users.noreply.github.com>
2024-09-28 21:10:42 +0530

    L243

--- coach/src/pages/coaches/CoachesList.vue
+++ coach/src/pages/coaches/CoachesList.vue
@@ -5,16 +5,26 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
     </ul>
     <h3 v-else>No Coaches found</h3>
   </section>
 </template>
 
 <script>
+import coachItem from "@/components/coaches/coachItem.vue";
 export default {
+  components: {
+    coachItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters["coaches/coaches"];
@@ -25,4 +35,15 @@ export default {
   },
 };
 </script>
-<style></style>
+<style>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
